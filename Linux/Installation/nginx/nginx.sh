f9d09b179a8be5c9373e5ffacacfc5da2e104113
deepaknreddy <104769185+deepaknreddy@users.noreply.github.com>
2022-06-01 14:04:24 +0530

    Add files via upload

---
+++ Linux/Installation/nginx/nginx.sh
@@ -0,0 +1,5 @@
+sudo yum update
+sudo yum install wget -y
+cd /etc/yum.repos.d/
+wget https://raw.githubusercontent.com/deepaknreddy/Learn/main/Linux/Installation/nginx/nginx.repo
+sudo yum install nginx
