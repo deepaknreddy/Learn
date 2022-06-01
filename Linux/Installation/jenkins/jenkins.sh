fcf7bdce60e8f9f6dbda161f0b4be4a20b0b9159
deepaknreddy <104769185+deepaknreddy@users.noreply.github.com>
2022-06-01 17:52:19 +0530

    Update jenkins.sh

--- Linux/Installation/jenkins/jenkins.sh
+++ Linux/Installation/jenkins/jenkins.sh
@@ -3,3 +3,4 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key    #key will be placed/copied to /var/lib/jenkins/secrets/initialAdminPassword
 sudo yum install fontconfig java-11-openjdk
 sudo yum install jenkins
+sudo service jenkins start
