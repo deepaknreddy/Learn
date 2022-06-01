2761cc603285a707d0844356e7f1f65a409e0d04
deepaknreddy <104769185+deepaknreddy@users.noreply.github.com>
2022-06-01 14:13:16 +0530

    Update nginx.sh

--- Linux/Installation/nginx/nginx.sh
+++ Linux/Installation/nginx/nginx.sh
@@ -2,4 +2,4 @@ sudo yum update -y
 sudo yum install wget -y
 cd /etc/yum.repos.d/
 wget https://raw.githubusercontent.com/deepaknreddy/Learn/main/Linux/Installation/nginx/nginx.repo
-sudo yum install nginx
+sudo yum install nginx -y
