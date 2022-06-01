aa0600b04a2ddd61c65bb5038cb46592c4ea8f31
deepaknreddy <104769185+deepaknreddy@users.noreply.github.com>
2022-06-01 17:00:37 +0530

    Update tomcat.sh

--- Linux/Installation/tomcat/tomcat.sh
+++ Linux/Installation/tomcat/tomcat.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+cd /opt
+sudo yum install wget unzip
+sudo wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.0.21/bin/apache-tomcat-10.0.21.zip
+sudo unzip apache-tomcat-10.0.21.zip
+sudo rm -rf apache-tomcat-10.0.21.zip
