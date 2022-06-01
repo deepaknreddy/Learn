d9897916e9c5f493d6059bda4ff431f0df64df0a
deepaknreddy <104769185+deepaknreddy@users.noreply.github.com>
2022-06-01 17:50:51 +0530

    Create jenkins.sh

---
+++ Linux/Installation/jenkins/jenkins.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key    #key will be placed/copied to /var/lib/jenkins/secrets/initialAdminPassword
+sudo yum install fontconfig java-11-openjdk
+sudo yum install jenkins
