9dc7468c94dbb77004a4b2d5c03fb71cd9f236dd
deepaknreddy <104769185+deepaknreddy@users.noreply.github.com>
2022-06-01 17:41:29 +0530

    Update tomcat.sh

--- Linux/Installation/tomcat/tomcat.sh
+++ Linux/Installation/tomcat/tomcat.sh
@@ -4,3 +4,10 @@ sudo yum install wget unzip
 sudo wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.0.21/bin/apache-tomcat-10.0.21.zip
 sudo unzip apache-tomcat-10.0.21.zip
 sudo rm -rf apache-tomcat-10.0.21.zip
+cd apache-tomcat-10.0.21/bin/
+sudo chmod u+x catalina.sh shutdown.sh  startup.sh
+sudo ln -s /opt/apache-tomcat-10.0.21/bin/startup.sh /usr/bin/tomcatup
+sudo yum install java
+sudo ln -s /opt/apache-tomcat-10.0.21/bin/shutdown.sh /usr/bin/tomcatdown
+sudo sed -i 's/8080/8081/g' /opt/apache-tomcat-10.0.21/conf/server.xml        #to change port associated with tomcat from 8080 to 8081 
+sudo tomcatup
