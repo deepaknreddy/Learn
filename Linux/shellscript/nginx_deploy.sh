a8c414a818834daeb3a78fb93c5dcc8ee61fa311
deepaknreddy <104769185+deepaknreddy@users.noreply.github.com>
2022-06-26 01:52:47 +0530

    Update nginx_deploy.sh

--- Linux/shellscript/nginx_deploy.sh
+++ Linux/shellscript/nginx_deploy.sh
@@ -1,6 +1,7 @@
 sudo service nginx stop
 sudo rm -rf /usr/share/nginx/html/*
 cd /opt
+sudo rm -rf REPO_NAME
 sudo yum install git -y
 sudo git clone REPO_GIT_URL
 sudo cp REPO_NAME/* /usr/share/nginx/html/
