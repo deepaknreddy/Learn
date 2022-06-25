32c70c963a3a277ffdf7c1598562b9a6487872d8
deepaknreddy <104769185+deepaknreddy@users.noreply.github.com>
2022-06-26 01:51:48 +0530

    Create httpd_deploy.sh

---
+++ Linux/shellscript/httpd_deploy.sh
@@ -0,0 +1,8 @@
+sudo service httpd stop
+sudo yum install git -y
+cd /opt
+sudo rm -rf REPO_NAME
+sudo git clone REPO_GIT_URL
+sudo rm -rf /var/www/html/*
+sudo cp -r REPO_NAME/* /var/www/html/
+sudo service httpd start
