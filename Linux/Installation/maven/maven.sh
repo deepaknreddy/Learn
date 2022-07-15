d0be4e57be44a6008a53120fc0ceae1921214211
deepaknreddy <104769185+deepaknreddy@users.noreply.github.com>
2022-07-15 17:41:02 +0530

    Add files via upload

---
+++ Linux/Installation/maven/maven.sh
@@ -0,0 +1,6 @@
+# Apache Maven Environmental Variables
+# MAVEN_HOME for Maven 1 - M2_HOME for Maven 2
+export JAVA_HOME=/usr/lib/jvm/jre-openjdk
+export M2_HOME=/opt/apache-maven-3.6.0
+export MAVEN_HOME=/opt/apache-maven-3.6.0
+export PATH=${M2_HOME}/bin:${PATH}
